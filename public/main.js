bfbcdc4bcc73f887f91cd60297f51b465e9590c0
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2020-08-25 16:18:59 -0500

    Cleaned up JS
    Add comments and rearranged some variables

--- public/main.js
+++ public/main.js
@@ -2,22 +2,24 @@ var menuButton = document.getElementById('side-bar-icon');
 var menu = document.getElementById('side-bar');
 var navLinks = document.getElementsByClassName('nav-link');
 var tabs = document.getElementsByClassName('tab-content');
+var options = {
+    wrapAround: true,
+    prevNextButtons: false
+}
 
-document.getElementById('side-bar-icon').addEventListener('click', () => {
+// Open and close the menu when the menu button is clicked
+menuButton.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
 
+// Close the menu when a nav link is clicked
 for(const link of navLinks) {
     link.addEventListener("click", () => {
         document.getElementById("side-bar").classList.toggle("active");
     });
 };
 
-var options = {
-    wrapAround: true,
-    prevNextButtons: false
-}
-
+// Change selected tab to display grid and all other to display none
 function openTab(projectNo) {
     for (const tab of tabs) {
         tab.style.display = 'none';
@@ -26,11 +28,11 @@ function openTab(projectNo) {
     var flkty = new Flickity(`#${projectNo}-carousel`, options);
     flkty.resize();
 }
-
+// Set initial tab-content to display grid and initialize carousel
 document.getElementById('project1').style.display = 'grid';
 var project1Carousel = new Flickity('#project1-carousel', options);
 
-
+// Change theme css variables
 function changeTheme(theme) {
     if(theme == 'zero-one') {
         document.documentElement.style.setProperty('--primary', 'var(--zero-one-primary)');
@@ -44,6 +46,7 @@ function changeTheme(theme) {
     }
 }
 
+// Testing when elements extend beyond screen width
 var docWidth = document.documentElement.offsetWidth;    
 [].forEach.call(
     document.querySelectorAll('*'),
